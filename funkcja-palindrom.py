9e5927c8c7795e827243fb616abd8eb50f258735
Kamil Prokop <zara.pug.yt@gmail.com>
2023-01-07 02:30:23 +0100

    defining function body

--- funkcja-palindrom.py
+++ funkcja-palindrom.py
@@ -1,4 +1,9 @@
 def palindrom(arg_1 = "palindrom"):
     middle_letter = len(arg_1) // 2
 
+    counting = 0
+    for i in range(0, int(middle_letter)):
+        if arg_1[i] == arg_1[len(arg_1) - 1 - i]:
+            counting +=1
+
 palindrom()
